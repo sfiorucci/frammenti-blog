a41682706d0efbbcd4c7db2141afeb443c2690e7
sfiorucci <simone@commercelayer.io>
2025-04-02 17:24:23 +0200

    Add tailwind CSS and remove old styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://parolemusica.it"
+  site: "https://parolemusica.it",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
